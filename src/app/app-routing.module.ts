2fd2382a5f72a2cb4a8f044130011dbf8d34104b
pliniopvv <gangss2@hotmail.com>
2022-06-10 08:58:11 -0300

    Cubo finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { CubeComponent } from './cube/cube.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'cube' },
+  { path: 'cube', component: CubeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
